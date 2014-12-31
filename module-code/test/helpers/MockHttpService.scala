2d87e2585850112bf5a3e3814c1639a0ffee1678
Mirko Köster <it@mirkokoester.de>
2014-12-31 23:51:10 +0100

    fixed deprecation warnings (plus automatic formatting)

--- module-code/test/helpers/MockHttpService.scala
+++ module-code/test/helpers/MockHttpService.scala
@@ -1,11 +1,11 @@
 package helpers
 
 import org.specs2.mock.Mockito
-import play.api.http.{ContentTypeOf, Writeable, HeaderNames}
+import play.api.http.{ ContentTypeOf, Writeable, HeaderNames }
 import play.api.libs.ws._
 import play.api.http.Status._
 import scala.concurrent.Future
-import play.api.libs.ws.WS.WSRequestHolder
+import play.api.libs.ws.WSRequestHolder
 import securesocial.core.services.HttpService
 import org.mockito.Answers.RETURNS_DEEP_STUBS
 import org.mockito.Mockito.withSettings
@@ -18,9 +18,9 @@ object MockHttpService {
 class MockHttpService extends Mockito with HttpService {
 
   val request = mock[WSRequestHolder].as(s"Request($hashCode)")
-  val response = mock[Response].as(s"Response($hashCode")
+  val response = mock[WSResponse].as(s"Response($hashCode")
 
-  val urls:collection.mutable.Buffer[String] = new collection.mutable.ArrayBuffer[String]()
+  val urls: collection.mutable.Buffer[String] = new collection.mutable.ArrayBuffer[String]()
 
   response.status returns OK
   response.header(HeaderNames.CONTENT_TYPE) returns Some("text/html;charset=UTF-8")
