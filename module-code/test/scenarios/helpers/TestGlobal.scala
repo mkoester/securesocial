a0db92c0bd3d3f4a3fa9d7b5f916eac354e65d39
Mirko Köster <it@mirkokoester.de>
2014-12-31 23:20:09 +0100

    moved classes from package object to package (fixing compiler warnings)

---
+++ module-code/test/scenarios/helpers/TestGlobal.scala
@@ -0,0 +1,28 @@
+package scenarios.helpers
+
+import java.lang.reflect.Constructor
+import securesocial.core.RuntimeEnvironment
+import securesocial.core.services.{ UserService, RoutesService }
+
+class TestGlobal[U](var runtimeEnvironment: RuntimeEnvironment[U]) extends play.api.GlobalSettings {
+
+  /**
+   * An implementation that checks if the controller expects a RuntimeEnvironment and
+   * passes the instance to it if required.
+   *
+   * This can be replaced by any DI framework to inject it differently.
+   *
+   * @param controllerClass
+   * @tparam A
+   * @return
+   */
+  override def getControllerInstance[A](controllerClass: Class[A]): A = {
+    val instance = controllerClass.getConstructors.find { c =>
+      val params = c.getParameterTypes
+      params.length == 1 && params(0) == classOf[RuntimeEnvironment[U]]
+    }.map {
+      _.asInstanceOf[Constructor[A]].newInstance(runtimeEnvironment)
+    }
+    instance.getOrElse(super.getControllerInstance(controllerClass))
+  }
+}
